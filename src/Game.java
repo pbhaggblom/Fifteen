ee8b6062a2024c8767361804aab56e4d6df68a9c
Kim Zugic <kim.zugic@live.com>
2024-11-03 13:36:18 +0100

    funktion för att starta nytt spel

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -113,6 +114,8 @@ public class Game extends JFrame implements ActionListener {
 
         if(currentLine.toString().contentEquals(GameWin.WINNING_LINE.win)) {
             JOptionPane.showMessageDialog(null, "Congratulations! You won!");
+            this.dispose();
+            new Main().newGame();
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import javax.swing.*;
+
 public class Main {
+
+    public void newGame(){
+        int startNew = JOptionPane.showOptionDialog(null,
+                "Do you want to start a new game?",
+                "New Game", JOptionPane.YES_NO_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,null,null);
+
+        if(startNew == JOptionPane.YES_OPTION){
+            new Game();
+        }else{
+            System.exit(0);
+        }
+    }
 
     public static void main(String[] args) {
 
-        Game g = new Game();
+        new Main().newGame();
     }
 }
